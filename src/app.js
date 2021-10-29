bd6f196525017050267b6383b26bf2b7b9b1472d
diegomarteens <diegomarteens@gmail.com>
2021-10-29 01:38:13 +0000

    newUpdate

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function genRandomExcuse() {
+    let who = ["The Cat", "My grandpa", "His Monkey", "My Dog"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the computer", "the bike"];
+    let when = [
+      "before the job",
+      "right on time",
+      "when I walked",
+      "during my dinner",
+      "while I was playing"
+    ];
+    var randomWho = who[Math.floor(Math.random() * who.length)];
+    var randomAction = action[Math.floor(Math.random() * action.length)];
+    var randomWhat = what[Math.floor(Math.random() * what.length)];
+    var randomWhen = when[Math.floor(Math.random() * when.length)];
+    return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  }
+  const renderExcuse = () => {
+    document.querySelector("#excuse").innerHTML = genRandomExcuse();
+  };
+  document.querySelector("#btn").addEventListener("click", renderExcuse);
+  renderExcuse();
 };
